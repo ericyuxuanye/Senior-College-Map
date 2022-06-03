2cb5edf51c840c2335b04b846b40678f6e84133d
Eric Ye <yuxuan.smiley@gmail.com>
2022-06-03 16:47:44 -0700

    Some changes
    The close button will always be on top, and some images were changed

--- src/App.js
+++ src/App.js
@@ -4,24 +4,64 @@ import ReactTooltip from "react-tooltip";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import React, { useState } from "react";
+import Popup from "reactjs-popup";
 
 function App() {
   const [content, setContent] = useState("");
+  const [people, setPeople] = useState("");
+  const [college, setCollege] = useState("");
+  const [image, setImage] = useState("");
+  const [open, setOpen] = useState(false);
+  const popup = (college, image, students) => {
+    let result = "";
+    if (students.length > 1) {
+      for (let i = 0; i < students.length - 1; i++) {
+        result += students[i] + ", ";
+      }
+      result += "and " + students[students.length - 1];
+    } else {
+      result = students[0];
+    }
+    setPeople(result);
+    setImage(image);
+    setCollege(college);
+    setOpen(true);
+  };
   return (
-    <Tabs>
-      <TabList>
-        <Tab>US Map</Tab>
-        <Tab>International Map</Tab>
-      </TabList>
-      <TabPanel>
-        <USMap setTooltipContent={setContent} />
-        <ReactTooltip>{content}</ReactTooltip>
-      </TabPanel>
-      <TabPanel>
-        <InternationalMap setTooltipContent={setContent} />
-        <ReactTooltip>{content}</ReactTooltip>
-      </TabPanel>
-    </Tabs>
+    <>
+      <Tabs>
+        <TabList>
+          <Tab>US Map</Tab>
+          <Tab>International Map</Tab>
+        </TabList>
+        <TabPanel>
+          <USMap setTooltipContent={setContent} popup={popup} />
+        </TabPanel>
+        <TabPanel>
+          <InternationalMap setTooltipContent={setContent} popup={popup} />
+        </TabPanel>
+      </Tabs>
+      <ReactTooltip>{content}</ReactTooltip>
+      <Popup
+        open={open}
+        modal
+        onClose={() => setOpen(false)}
+        position="right center"
+      >
+        {(close) => (
+          <>
+            <div className="popup-text">
+              <h3>{college}</h3>
+              <img src={image} width="500" alt={college} />
+              <p>Congratulations {people}!</p>
+            </div>
+            <button className="popup-close" onClick={close}>
+              &#x2715;
+            </button>
+          </>
+        )}
+      </Popup>
+    </>
   );
 }
 
--- src/InternationalMap.js
+++ src/InternationalMap.js
@@ -6,130 +6,88 @@ import {
   Marker,
   ZoomableGroup,
 } from "react-simple-maps";
-import Popup from "reactjs-popup";
 import us_colleges from "./us_colleges.json";
 import international_colleges from "./international_colleges.json";
 
 const colleges = us_colleges.concat(international_colleges);
 
-const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-const InternationlMap = ({ setTooltipContent }) => {
+const InternationlMap = ({ setTooltipContent, popup }) => {
   const [scaleFactor, setScaleFactor] = useState(1);
-  const [open, setOpen] = useState(false);
-  const [people, setPeople] = useState("");
-  const [college, setCollege] = useState("");
-  const [image, setImage] = useState("");
-  const updatePeople = (students) => {
-    let result = "";
-    if (students.length > 1) {
-      for (let i = 0; i < students.length - 1; i++) {
-        result += students[i] + ", ";
-      }
-      result += "and " + students[students.length - 1];
-    } else {
-      result = students[0];
-    }
-    setPeople(result);
-    setOpen(true);
-  };
   return (
-    <>
-      <ComposableMap data-tip="" style={{outline: "none"}}>
-        <ZoomableGroup onMove={({ k }) => setScaleFactor(k)} maxZoom={20}>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) => (
-              <>
-                {geographies.map((geo) => (
-                  <Geography
-                    key={geo.rsmKey}
-                    stroke="#FFF"
-                    geography={geo}
-                    fill="#DDD"
-                    onMouseEnter={() => {
+    <ComposableMap data-tip="" style={{ outline: "none" }}>
+      <ZoomableGroup onMove={({ k }) => setScaleFactor(k)} maxZoom={20}>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) => (
+            <>
+              {geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  stroke="#FFF"
+                  geography={geo}
+                  fill="#DDD"
+                  onMouseEnter={() => {
                     const { NAME } = geo.properties;
                     setTooltipContent(NAME);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#DDD",
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#95c99f",
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#1cb828",
+                      outline: "none",
+                    },
+                  }}
+                />
+              ))}
+              {colleges.map(({ college, lat, lng, image, students }) => {
+                return (
+                  <Marker
+                    onMouseEnter={() => {
+                      setTooltipContent(
+                        <>
+                          <p className="text_center">
+                            <b>{college}</b>
+                          </p>
+                          <img
+                            src={image}
+                            width="200"
+                            alt={college}
+                            className="center"
+                          />
+                        </>
+                      );
                     }}
-                    onMouseLeave={() => {
-                      setTooltipContent("");
-                    }}
-                    style={{
-                      default: {
-                        fill: "#DDD",
-                        outline: "none",
-                      },
-                      hover: {
-                        fill: "#95c99f",
-                        outline: "none",
-                      },
-                      pressed: {
-                        fill: "#1cb828",
-                        outline: "none",
-                      },
-                    }}
-                  />
-                ))}
-                {colleges.map(({ college, lat, lng, image, students }) => {
-                  return (
-                    <Marker
-                      onMouseEnter={() => {
-                        setTooltipContent(
-                          <>
-                            <p className="text_center">
-                              <b>{college}</b>
-                            </p>
-                            <img
-                              src={image}
-                              width="200"
-                              alt={college}
-                              className="center"
-                            />
-                          </>
-                        );
-                      }}
-                      onMouseLeave={() => {
-                        setTooltipContent("");
-                      }}
-                      onClick={() => {
-                        updatePeople(students);
-                        setCollege(college);
-                        setImage(image);
-                      }}
-                      coordinates={[lng, lat]}
-                      key={college}
-                    >
-                      <circle
-                        style={{ cursor: "pointer" }}
-                        r={5 / scaleFactor}
-                        fill="#F00"
-                        fillOpacity="0.5"
-                      />
-                    </Marker>
-                  );
-                })}
-              </>
-            )}
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-      <Popup
-        open={open}
-        modal
-        onClose={() => setOpen(false)}
-        position="right center"
-      >
-        {(close) => (
-          <div style={{ position: "relative", textAlign: "center" }}>
-            <h3>{college}</h3>
-            <img src={image} width="500" alt={college}/>
-            <p>Congratulations {people}!</p>
-            <button className="popup-close" onClick={close}>
-              ✕
-            </button>
-          </div>
-        )}
-      </Popup>
-    </>
+                    onMouseLeave={() => setTooltipContent("")}
+                    onClick={() => popup(college, image, students)}
+                    coordinates={[lng, lat]}
+                    key={college}
+                  >
+                    <circle
+                      style={{ cursor: "pointer" }}
+                      r={5 / scaleFactor}
+                      fill="#F00"
+                      fillOpacity="0.5"
+                    />
+                  </Marker>
+                );
+              })}
+            </>
+          )}
+        </Geographies>
+      </ZoomableGroup>
+    </ComposableMap>
   );
 };
 
